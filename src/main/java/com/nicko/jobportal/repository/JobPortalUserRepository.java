a0b7fbeab9c0ce3e359f39c35391e04e6b7a5561
Nicholas <nicholasmurimi254@gmail.com>
2026-06-06 10:16:27 +0300

    add jobportal user repository

---
+++ src/main/java/com/nicko/jobportal/repository/JobPortalUserRepository.java
@@ -0,0 +1,14 @@
+package com.nicko.jobportal.repository;
+
+import com.nicko.jobportal.entity.JobPortalUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface JobPortalUserRepository extends JpaRepository<JobPortalUser, Long> {
+
+    Optional<JobPortalUser> readUserByEmailOrMobileNumber(String email, String mobileNumber);
+
+    Optional<JobPortalUser> findJobPortalUserByEmail(String email);
+
+}
